e82e1d30045fa3628baaf58e1a6c7b7bd61e8671
Ildar <misterildar@gmail.com>
2023-05-17 10:29:19 +0300

    10:31

--- src/components/index.js
+++ src/components/index.js
@@ -1,22 +1,19 @@
 import('../pages/index.css');
 
+
+function openAccordion(parent) {
+  if (parent.classList.contains('accordion__item-active')) {
+    parent.classList.remove('accordion__item-active')
+  } else {
+    document.querySelectorAll('.accordion__item').forEach((child) =>
+      child.classList.remove('accordion__item-active'))
+    parent.classList.add('accordion__item-active')
+  }
+}
+
 document.querySelectorAll('.accordion__item-container').forEach((item) =>
   item.addEventListener('click', () => {
     const parent = item.parentNode
-    if (parent.classList.contains('accordion__item-active')) {
-      parent.classList.remove('accordion__item-active')
-
-    } else {
-      document.querySelectorAll('.accordion__item').forEach((child) =>
-        child.classList.remove('accordion__item-active'))
-      parent.classList.add('accordion__item-active')
-    }
+    openAccordion(parent);
   })
 )
-
-// Так можно открыть все блоки
-// document.querySelectorAll('.accordion__item-triger').forEach((item) =>
-//   item.addEventListener('click', () => {
-//     item.parentNode.classList.toggle('accordion__item-active')
-//   })
-// )
