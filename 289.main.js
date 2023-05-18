7f9c72fa1184e1b3a0044599bc4e3965546b3d90
Anastasia Proskuryakova <anproskuryakova@ozon.ru>
2023-05-18 23:58:19 +0600

    Updates

---
+++ 289.main.js
@@ -0,0 +1,13 @@
+"use strict";
+(self["webpackChunkeverland"] = self["webpackChunkeverland"] || []).push([[289],{
+
+/***/ 289:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+}]);
