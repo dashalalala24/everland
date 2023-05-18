e79d253ae17e4e81239ce902e94a82584c2dcad6
Himany <Himaaan@yandex.ru>
2023-05-18 18:01:18 +0300

    dev/pay-form add validator

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,7 @@
 import('../pages/index.css');
+
+import FormValidator from './FormValidator.js';
+import {formPay, formSettings} from '../utils/constants.js';
+
+const formPayValidator = new FormValidator(formPay, formSettings);
+formPayValidator.enableValidation();
