cff9939aca52c89f74b49684e0dd1bbb1eea097c
VLGrigoryan <v.grigoryan@live.com>
2023-05-19 15:55:14 +0200

    Slider

--- src/components/index.js
+++ src/components/index.js
@@ -1,45 +1,184 @@
 import('../pages/index.css');
 
+// const slider = document.querySelector('.slider');
+// const sliderList = slider.querySelector('.slider__list');
+// const slides = slider.querySelectorAll('.slider__slide');
+// const prevButton = slider.querySelector('.button_type_slider_previous');
+// const nextButton = slider.querySelector('.button_type_slider_next');
 
-const slider = document.querySelector('.slider__list');
-const previousButton = document.querySelector('.button_type_slider_previous');
-const nextButton = document.querySelector('.button_type_slider_next');
-const slideWidth = slider.offsetWidth;
+const sliderList = document.querySelector('.slider__list');
+const slide = document.querySelectorAll('.slider__slide');
+const slideLength = slide.length;
 
-previousButton.addEventListener('click', () => {
-  slider.style.transform = `translateX(-${slideWidth}px)`;
-  slider.style.transition = 'transform 0.5s ease-in-out';
+const nextButton = document.querySelector(".button_type_slider_next");
+const previousButton = document.querySelector(".button_type_slider_previous");
+let slider = [];
 
-  setTimeout(() => {
-    slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
-    slider.style.transform = 'translateX(0)';
-    slider.style.transition = 'none';
-  }, 500);
-});
+for (let i = 0; i < slideLength; i++) {
+  slider[i] = slide[i];
+  slide[i].remove();
+}
 
-nextButton.addEventListener('click', () => {
-  slider.style.transform = `translateX(${slideWidth}px)`;
-  slider.style.transition = 'transform 0.5s ease-in-out';
+let step = 0;
+let offset = 0;
 
-  setTimeout(() => {
-    slider.appendChild(slider.firstElementChild);
-    slider.style.transform = 'translateX(0)';
-    slider.style.transition = 'none';
-  }, 600);
-});
-function openAccordion(parent) {
-  if (parent.classList.contains('accordion__item-active')) {
-    parent.classList.remove('accordion__item-active')
+function infoSlider() {
+  let elementSlide = document.createElement('li');
+  elementSlide = slider[slider.length - 1];
+  elementSlide.classList.add('slider__slide');
+  elementSlide.style.left = -1360 + 'px';
+  sliderList.appendChild(elementSlide);
+
+  elementSlide = slider[step];
+  elementSlide.classList.add('slider__slide');
+  elementSlide.style.left = offset * 1360 + 'px';
+  sliderList.appendChild(elementSlide);
+
+  elementSlide = slider[step + 1];
+  elementSlide.classList.add('slider__slide');
+  elementSlide.style.left = offset*1360 + 1360 + 'px';
+  sliderList.appendChild(elementSlide);
+
+  offset = 1;
+}
+
+function infoSliderPrevious() {
+  if (step == 0) {
+    step = slider.length - 2;
   } else {
-    document.querySelectorAll('.accordion__item').forEach((child) =>
-      child.classList.remove('accordion__item-active'))
-    parent.classList.add('accordion__item-active')
+    if (step == 1) {
+      step = slider.length - 1;
+    } else {
+      step = step - 2;
+    }
+  }
+
+  let offset = -1;
+  let elementSlide = document.createElement('li');
+  elementSlide = slider[step];
+  elementSlide.classList.add('slider__slide');
+  elementSlide.style.left = offset*1360 + 'px';
+  sliderList.insertBefore(elementSlide, sliderList.firstElementChild);
+
+  if (step == slider.length - 1) {
+    step = 0;
+  } else {
+    step = step + 1;
+  }
+  
+  offset = 1;
+
+}
+ 
+
+function previous() {
+  previousButton.onclick = null;
+  let slider2 = document.querySelectorAll('.slider__slide');
+  let offset2 = -1;
+
+  for (let i = 0; i < slider2.length; i++) {
+    slider2[i].style.left = offset2*1360 - 1360 + 'px';
+    offset2++;
+  }
+
+
+  setTimeout(function () {
+    slider2[0].remove();
+    infoSliderPrevious();
+    previousButton.onclick = previous;
+  }, 400);
+}
+
+function infoSliderNext() {
+ 
+  if (step == slider.length - 1) {
+    step = 1;
+  } else {
+    if (step == slider.length - 2) {
+      step = 0;
+    } else {
+      step = step + 2;
+    }
+  }
+
+  let elementSlide = document.createElement('li');
+  elementSlide = slider[step];
+  elementSlide.classList.add('slider__slide');
+  elementSlide.style.left = offset*1360 + 'px';
+  sliderList.appendChild(elementSlide);
+
+  if (step == 0) {
+    step = slider.length - 1;
+  } else {
+    step = step - 1;
+  }
+
+  offset = 1;
+}
+
+function next() {
+  nextButton.onclick = null;
+
+  let slider2 = document.querySelectorAll('.slider__slide');
+  let offset2 = slider2.length - 1;
+
+  for (let i = slider2.length - 1; i >= 0; i--) {
+    slider2[i].style.left = offset2*1360 + 'px';
+    offset2--;
+  }
+
+  setTimeout(function () {
+    slider2[slider2.length - 1].remove();
+    infoSliderNext();
+    nextButton.onclick = next;
+  }, 400);
+}
+
+infoSlider();
+step = 0;
+
+previousButton.onclick = previous;
+nextButton.onclick = next;
+
+// const slider = document.querySelector('.slider__list');
+// const previousButton = document.querySelector('.button_type_slider_previous');
+// const nextButton = document.querySelector('.button_type_slider_next');
+// const slideWidth = slider.offsetWidth;
+
+// previousButton.addEventListener('click', () => {
+//   slider.style.transform = `translateX(-${slideWidth}px)`;
+//   slider.style.transition = 'transform 0.5s ease-in-out';
+
+//   setTimeout(() => {
+//     slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
+//     slider.style.transform = 'translateX(0)';
+//     slider.style.transition = 'none';
+//   }, 500);
+// });
+
+// nextButton.addEventListener('click', () => {
+//   slider.style.transform = `translateX(${slideWidth}px)`;
+//   slider.style.transition = 'transform 0.5s ease-in-out';
+
+//   setTimeout(() => {
+//     slider.appendChild(slider.firstElementChild);
+//     slider.style.transform = 'translateX(0)';
+//     slider.style.transition = 'none';
+//   }, 600);
+// });
+function openAccordion(parent) {
+  if (parent.classList.contains('accordion__slide-active')) {
+    parent.classList.remove('accordion__slide-active')
+  } else {
+    document.querySelectorAll('.accordion__slide').forEach((child) =>
+      child.classList.remove('accordion__slide-active'))
+    parent.classList.add('accordion__slide-active')
   }
 }
 
-document.querySelectorAll('.accordion__item-container').forEach((item) =>
-  item.addEventListener('click', () => {
-    const parent = item.parentNode
+document.querySelectorAll('.accordion__slide-container').forEach((slide) =>
+  slide.addEventListener('click', () => {
+    const parent = slide.parentNode
     openAccordion(parent);
   })
 )
