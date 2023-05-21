89d480bc79a2e7f823b1d4bbff591f70bbb564c2
Himany <Himaaan@yandex.ru>
2023-05-21 19:30:48 +0300

    revert js code

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ import('./menu');
 import('./navigation')
 
 import ProjectSlider from './ProjectSlider.js';
+
 import FormValidator from './FormValidator.js';
 import FormPay from './FormPay.js';
 import { formPaySelector, formSettings } from '../utils/constants.js';
@@ -15,3 +16,38 @@ formPayValidator.enableValidation();
 
 const projectSlider = new ProjectSlider('.slider');
 projectSlider.setEventListeners();
+
+
+/*
+  Аккордион
+*/
+function changeButton(button) {
+  if (button.classList.contains('button_type_accordeon-checked')) {
+    button.classList.remove('button_type_accordeon-checked')
+  } else {
+    document.querySelectorAll('.button_type_accordeon').forEach((el) =>
+      el.classList.remove('button_type_accordeon-checked'))
+    button.classList.add('button_type_accordeon-checked')
+  }
+}
+
+function openAccordion(parent) {
+  if (parent.classList.contains('accordion__item-active')) {
+    parent.classList.remove('accordion__item-active')
+  } else {
+    document.querySelectorAll('.accordion__item').forEach((child) =>
+      child.classList.remove('accordion__item-active'))
+    parent.classList.add('accordion__item-active')
+  }
+}
+
+document.querySelectorAll('.accordion__item-container').forEach((item) =>
+  item.addEventListener('click', () => {
+    const parent = item.parentNode
+    const button = item.querySelector('.button_type_accordeon')
+    openAccordion(parent);
+    changeButton(button)
+  })
+)
+
+scrollToAnchors()
